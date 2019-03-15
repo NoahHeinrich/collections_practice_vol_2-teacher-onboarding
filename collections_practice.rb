06ccbb0d0f463f60e687955df9ce66e3eb1b94a7
Noa Heinrich <noah.heinrich@flatironschool.com>
2019-03-15 16:36:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,7 @@
 # your code goes here
 def begins_with_r(array)
   array.each do |tool|
-    return false unless tool.starts_with?("r")
+    return false unless tool.start_with?("r")
   end
   true
 end
